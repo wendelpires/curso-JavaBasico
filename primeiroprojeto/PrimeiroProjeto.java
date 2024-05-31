11d9d80e243a347382f048de4acf183bc1df20b6
Wendel Pires da Costa <wendel.pires94@gmail.com>
2024-05-31 08:47:48 -0300

    Commit first project in Java
    Hello World!

---
+++ primeiroprojeto/PrimeiroProjeto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package primeiroprojeto.primeiroprojeto;
+
+/**
+ *
+ * @author wende
+ */
+public class PrimeiroProjeto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
